9f3c148df2479b65813bb7fd7769d9f0a7fc3100
Edy Hernandez <edy.hernandez@williamoneil.com>
2016-12-28 11:46:06 -0800

    added further dependencies related to the last big update to the theme

---
+++ footer-home.php
@@ -0,0 +1,25 @@
+    </div>
+    				</div>
+
+					<footer class="blog-footer footer-down m-header scene_element scene_element--fadein">
+       					<?php the_field('footer', 'option'); ?>
+    				</footer>
+    	    </div> <!-- /.off-canvas-content -->
+  		</div> <!-- /.off-canvas-wrapper-inner -->
+  	</div> <!-- /. off-canvas-wrapper -->
+
+
+    <script>
+    	$(document).foundation();
+    </script>	
+    <script src="wp-content/themes/heart-with-feet/js/jquery.imagesloaded.min.js"></script>
+	<script src="wp-content/themes/heart-with-feet/js/cbpBGSlideshow.min.js"></script>
+	<script src="wp-content/themes/heart-with-feet/js/modernizr.custom.js"></script>
+	<script>
+		$(function() {
+			cbpBGSlideshow.init();
+		});
+	</script>
+	<?php wp_footer(); ?> 
+  </body>
+</html>
